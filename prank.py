fc3c107b572987ee7f8a4bbd78066c1bf0c887f7
Xander Taylor <xanderoid@gmail.com>
2014-07-16 10:27:53 -0700

    ran script, stripped numbers from pic filenames

---
+++ prank.py
@@ -0,0 +1,12 @@
+import os
+# define function to rename files
+def rename_files():
+	#(1) get file names from a folder
+	file_list = os.listdir(r'/Users/devbyxan/Documents/dev resources/python/udacityProgramingFoundations/prank')
+	
+	#(2) for each file, rename filename
+	os.chdir(r'/Users/devbyxan/Documents/dev resources/python/udacityProgramingFoundations/prank')
+	for file_name in file_list:
+		os.rename(file_name, file_name.translate(None, '0123456789'))
+		#translate method removes the characters in the 2nd arg
+rename_files()
