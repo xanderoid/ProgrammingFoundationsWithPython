bf10c5624bf21613530157c5d0719977ee1c8bef
Xander Taylor <xanderoid@gmail.com>
2014-07-16 09:48:24 -0700

    made while loop so break & browser open happen 3 times

--- Lesson01.py
+++ Lesson01.py
@@ -1,7 +1,10 @@
 import time
 import webbrowser
 
+# i is counter for program, initialized to zero
 i = 0
+
+print ("This program started on " + time.ctime())
 while (i < 3):
 	time.sleep(10)
 	webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
