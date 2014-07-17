71d2ee1b67ceea7d79fb9f3e22e50f5fe578fb50
Xander Taylor <xanderoid@gmail.com>
2014-07-17 12:51:32 -0700

    draw_art creates cirle from 36 squares with 10deg rotation

--- turtles.py
+++ turtles.py
@@ -1,38 +1,41 @@
 import turtle
 
-def draw_shapes():
+def draw_square(some_turtle):
+	for i in range(1,5):
+		some_turtle.fd(100)
+		some_turtle.rt(90)
+
+def draw_art():
 	window = turtle.Screen()
 	window.bgcolor("red")
 
 	jack = turtle.Turtle()
 	jack.shape("turtle")
 	jack.color("green")
-	jack.speed(1)
+	jack.speed(10)
 
-	i = 0
-	while (i<4):
-		jack.fd(100)
-		jack.rt(90)
-		i += 1
+	for i in range (1,37):
+		draw_square(jack)
+		jack.right(10)
 
-	jill = turtle.Turtle()
-	jill.shape("circle")
-	jill.color("yellow")
-	jill.speed(1)
+	# jill = turtle.Turtle()
+	# jill.shape("circle")
+	# jill.color("yellow")
+	# jill.speed(1)
 
-	jill.circle(100)
+	# jill.circle(100)
 
-	junior = turtle.Turtle()
-	junior.shape("triangle")
-	junior.color("black")
-	junior.speed(1)
+	# junior = turtle.Turtle()
+	# junior.shape("triangle")
+	# junior.color("black")
+	# junior.speed(1)
 
-	i = 0
-	while (i<3):
-		junior.fd(100)
-		junior.rt(120)
-		i += 1
+	# i = 0
+	# while (i<3):
+	# 	junior.fd(100)
+	# 	junior.rt(120)
+	# 	i += 1
 
 	window.exitonclick()
 
-draw_shapes()
+draw_art()
